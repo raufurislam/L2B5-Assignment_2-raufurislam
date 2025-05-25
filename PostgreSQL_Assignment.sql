07e16546a000c226b4bd3538d73a43230c9c3d31
raufurislam <raufurislam@gmail.com>
2025-05-25 17:58:52 +0600

    P2

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -12,18 +12,42 @@ INSERT INTO rangers (name, region) VALUES
 ('Carol King', 'Mountain Range');
 
 
--- Problem- 1️⃣ Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
+-- Problem 1️⃣ Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
 INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
 SELECT * FROM rangers;
+DROP TABLE rangers;
 DELETE FROM rangers WHERE ranger_id = 4;
 
+-- Problem 2️⃣ Count unique species ever sighted.
+CREATE Table species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) CHECK (conservation_status IN ('Endangered', 'Vulnerable', 'Historic'))
+);
+INSERT INTO species(common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species(species_id),
+    ranger_id INT REFERENCES rangers(ranger_id),
+    location VARCHAR(100) NOT NULL,
+    sighting_time TIMESTAMP NOT NULL,
+    notes TEXT
+);
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
 
+-- Count unique species ever sighted.
+SELECT count(DISTINCT species_id) AS unique_species_count FROM sightings;
 
--- CREATE Table species (
---     species_id SERIAL PRIMARY KEY,
---     species_id VARCHAR(50) NOT NULL,
---     scientific_name VARCHAR(100) NOT NULL,
---     discovery_date DATE NOT NULL,
---     conservation_status 
--- )
+SELECT * FROM species;
+SELECT * FROM sightings;
